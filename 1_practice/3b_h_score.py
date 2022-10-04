2875e220e41df22ebc3929496f7d2dbc449108e5
Jered <JeredL@gmail.com>
2022-10-04 09:03:59 -0500

    Practice 1 update

--- 1_practice/3b_h_score.py
+++ 1_practice/3b_h_score.py
@@ -4,12 +4,32 @@ JD Linares
 20220922
 '''
 
-for case_i in range(int(input())):
-    input()
-#    data = [int(i) for i in input().split()]
-    data = input().split()
+import pdb
+import heapq    
+    # heapify(x)            turn list x into a heap
+    # heappush(x, item)     add item to the heap
+    # heappop(x)            pop and return smallest item
 
-    print(data)
+for case_i in range(int(input())):
+
+    # Length not needed
+    input()     
+
+    data = [int(i) for i in input().split()]
+    heap_data = []
+    
+    h_index = 0
+    result = ""
+    for i in range(len(data)):
+        if data[i] > h_index:
+            heapq.heappush(heap_data,data[i])
+        while heap_data and heap_data[0] <= h_index:
+            heapq.heappop(heap_data)
+        if len(heap_data) > h_index:
+            h_index += 1
+        result += " " + str(h_index)
+
+    print(f"Case #{case_i+1}:{result}")
 
 
 
